52723c5e098c4398d67e6cea613c44029630278e
José Cubas <58412645+Splitzer26@users.noreply.github.com>
2024-07-04 10:47:26 -0600

    feat: new login page and changed the navbar

--- src/components/main-layout/navbar.tsx
+++ src/components/main-layout/navbar.tsx
@@ -1,37 +1,107 @@
-import Link from "next/link"
+'use client'
+import React from "react";
 import Image from "next/image";
 import Searcher from "./searcher";
 import BurguerMenu from "./burger-menu";
 import { ChatBubbleLeftRightIcon } from "@heroicons/react/24/outline";
-const Navbar = () => {
+import {
+    Navbar, 
+    NavbarBrand, 
+    NavbarContent, 
+    NavbarItem, 
+    NavbarMenuToggle,
+    NavbarMenu,
+    NavbarMenuItem,
+    Link,
+    Button
+  } from "@nextui-org/react";
+
+  export default function MainNavbar() {
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+    const menuItems = [
+        { url: "/", text: "Inicio" },
+        { url: "/shop", text: "Tienda" },
+        { url: "/services", text: "Servicios" },
+        { url: "/about-us", text: "Quienes Somos" },
+        { url: "/contact", text: "Contacto" },
+        { url: "/login", text: "Login" },
+        { url: "/logout", text: "Cerrar Sesion" },
+    ];
     // bg-red-400 sm:bg-green-300 md:bg-blue-500  lg:bg-amber-400 mx-auto xl:bg-red-300 2xl:bg-sky-500  z-50 sticky top-0 shadow-xs shadow-gray-300
     return (
-        <nav className="border-b flex justify-between  bg-[#F6F6F8]">
-            <Link href='/'>
-            <Image src='/assets/images/corporate/smart.webp' height={50} width={50} alt="smart business logo" /> 
-            </Link>
-            <div className="border rounded grow mx-4 md:mx-12 my-1 hidden lg:block border-gray-400">
-                <Searcher/>
-            </div>
-            <div className="grow flex justify-end my-1 p-1 sm:hidden  items-center rounded-md">
-                
-                <Link className="border-green-500 border-2 px-2 py-1 flex items-center gap-2 rounded-md text-gray-700 font-medium" href='https://api.whatsapp.com/send?phone=+50488187765&text=Deseo%20contactarme%20con%20un%20asesor%20de%20ventas'>
-                    <ChatBubbleLeftRightIcon className="text-green-600" width={25} height={25} />
-                    Contactar
-                </Link>
-            </div>
-            <div className="sm:flex gap-4 hidden items-center justify-end px-10">
-                <div className="text-gray-500 hover:text-blue-500 font-medium"><Link href="/">Inicio</Link></div>
-                <div className="text-gray-500 hover:text-blue-500 font-medium"><Link href="/shop">Tienda</Link></div>
-                <div className="text-gray-500 hover:text-blue-500 font-medium"><Link href="/services">Servicios</Link></div>
-                <div className="text-gray-500 hover:text-blue-500 font-medium"><Link href="/about-us">Quienes Somos</Link></div>
-                <div className="text-gray-500 hover:text-blue-500 font-medium"><Link href="/contact">Contacto</Link></div>
-            </div>
-            <div className=" flex justify-end my-1 p-1 sm:hidden  items-center rounded-md">
-                <BurguerMenu/>
-            </div>
-            {/* <LoginToggle/> */}
-        </nav>
+        <Navbar
+            isBordered
+            isMenuOpen={isMenuOpen}
+            onMenuOpenChange={setIsMenuOpen}
+        >
+            <NavbarContent className="sm:hidden" justify="start">
+                <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
+            </NavbarContent>
+
+            <NavbarContent className="sm:hidden pr-3" justify="center">
+                <NavbarBrand>
+                    <Image src='/assets/images/corporate/smart.webp' height={50} width={50} alt="smart business logo" />
+                </NavbarBrand>
+            </NavbarContent>
+
+            <NavbarContent className="hidden sm:flex gap-4" justify="center">
+                <NavbarBrand>
+                    <Image src='/assets/images/corporate/smart.webp' height={50} width={50} alt="smart business logo" />
+                </NavbarBrand>
+                <NavbarItem>
+                    <Link color="foreground" href="/">
+                        Inicio
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link color="foreground"  href="/shop" >
+                        Tienda
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link color="foreground" href="/services">
+                        Servicios
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link color="foreground" href="/about-us">
+                        Quienes Somos
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link color="foreground" href="/contact">
+                        Contacto
+                    </Link>
+                </NavbarItem>
+            </NavbarContent>
+
+            <NavbarContent justify="end">
+                <NavbarItem className="hidden lg:flex">
+                    <Link href="/login">Login</Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Button as={Link} color="warning" href="/sign-up" variant="flat">
+                        Registrarme
+                    </Button>
+                </NavbarItem>
+            </NavbarContent>
+
+            <NavbarMenu>
+                {menuItems.map((item, index) => (
+                    <NavbarMenuItem key={`${item.text}-${index}`}>
+                        <Link
+                            className="w-full"
+                            color={
+                                index === 2 ? "warning" : index === menuItems.length - 1 ? "danger" : "foreground"
+                            }
+                            href={item.url}
+                            size="lg"
+                        >
+                            {item.text}
+                        </Link>
+                    </NavbarMenuItem>
+                ))}
+            </NavbarMenu>
+        </Navbar>
     )
 }
-export default Navbar;
