7f5ea2a776c6681c88c362366a1f3e0263591f6e
Jose Cubas <jose.cubasz26@outlook.com>
2025-09-12 21:02:39 -0600

    fix: nextauth_secret

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,8 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || (process.env.NODE_ENV === 'development' ? 'https://localhost:7211/api/v2' : 'https://sb8.azurewebsites.net/api/v2'),
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL || (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'https://www.smartbusiness.site'),
     VERCEL_URL: process.env.VERCEL_URL,
+    NEXTAUTH_URL: process.env.NEXT_PUBLIC_APP_URL || (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'https://www.smartbusiness.site'),
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET || '11ce0ae293280fe23bfe6d7129f5cd07'
   },
   logging: {
     fetches: {
